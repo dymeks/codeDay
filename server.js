37d9a346496fff0b74de8dac193909a07e408e69
Michael Konvalinka <MikeKonvalinka@gmail.com>
2018-06-02 14:42:46 -0700

    Build first route for map rendering/testing connection

--- server.js
+++ server.js
@@ -2,14 +2,21 @@ const express = require('express');
 const bp = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const session = require('express-session');
+
 
 const app = express();
+app.set('view engine', 'ejs')
 
 app.use(bp.json());
 // app.use(express.static(path.join(__dirname,'./client/dist/client')));
 // app.set('views',path.join(__dirname,'./client/views'));
 // app.use(session({secret: "Shh, It's a secret!"}))
 // require('./server/config/routes.js')(app);
+
+app.get('/dashboard', function (req, res) {
+    res.render('map');
+  })
 
 app.listen(8000,function(){
     console.log("listening on port 8000");
